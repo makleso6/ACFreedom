26ee4db811502cc3ca29e60a7c6990a99afd11a4
Maxim Kolesnik <kolesnik@yandex-team.ru>
2022-07-06 14:42:01 +0300

    update

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,10 +10,11 @@ let package = Package(
         .library(
             name: "ACFreedom",
             targets: ["ACFreedom"]),
+        
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.13.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.40.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0"),
         .package(url: "https://github.com/Flight-School/AnyCodable.git", from: "0.2.3")
         // Dependencies declare other packages that this package depends on.
@@ -21,9 +22,16 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+//        "NIO", "Logging", "AnyCodable", "CryptoSwift"
         .target(
             name: "ACFreedom",
-            dependencies: ["NIO", "Logging", "AnyCodable", "CryptoSwift"]),
+            dependencies: [
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "CryptoSwift", package: "swift-log"),
+                .product(name: "Logging", package: "swift-nio"),
+                .product(name: "AnyCodable", package: "AnyCodable"),
+
+            ]),
         .testTarget(
             name: "ACFreedomTests",
             dependencies: ["ACFreedom"]),
